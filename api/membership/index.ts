8ec46d22a3bc46930e4c604ff2b51c655478360b
Zeyu Zhang <39144422+zeyu2001@users.noreply.github.com>
2024-08-03 00:09:52 +0800

    fix: routing

--- api/membership/index.ts
+++ api/membership/index.ts
@@ -1,32 +1,28 @@
-import type { NextFunction, Request, Response } from "express";
+import type { Request, Response } from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 
 const apiProxy = createProxyMiddleware<Request, Response>({
   target: process.env.MEMBERSHIP_API_GATEWAY,
   changeOrigin: true,
   pathRewrite: {
-    "^/api/membership": "", // strip "/api" from the URL
+    "^/membership": "",
+  },
+  headers: {
+    "X-Api-Key": process.env.MEMBERSHIP_API_KEY,
   },
   on: {
-    proxyReq: (proxyReq, req, res) => {
-      if (!process.env.MEMBERSHIP_API_KEY) {
-        throw new Error("MEMBERSHIP_API_KEY is not set");
-      }
-      proxyReq.setHeader("X-Api-Key", process.env.MEMBERSHIP_API_KEY);
-    },
     proxyRes: (proxyRes, req, res) => {
-      proxyRes.headers["Access-Control-Allow-Origin"] = req.headers.origin;
-      proxyRes.headers["Access-Control-Allow-Credentials"] = "true";
       for (const key in proxyRes.headers) {
-        if (key.startsWith("x-amz")) {
+        if (key.toLowerCase().startsWith("x-amz")) {
           delete proxyRes.headers[key];
         }
       }
+      proxyRes.headers["Access-Control-Allow-Origin"] = req.headers.origin;
+      proxyRes.headers["Access-Control-Allow-Credentials"] = "true";
     },
   },
 });
 
-// Expose the proxy on the "/api/*" endpoint.
 const handler = (req: Request, res: Response) => {
   return apiProxy(req, res);
 };
